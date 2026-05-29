492ac0cdce1b8942d1e94d8531086ff02648e6af
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-29 10:45:19 +0100

    gigs test passes -- temporal date converted in browser but not test?

--- app.test.js
+++ app.test.js
@@ -6,7 +6,7 @@ const gigs = [
         name: "Blood Orange",
         image: "https://media.vogue.co.uk/photos/5f7af392a67cbe91f90e282b/2:3/w_2560%2Cc_limit/shutterstock_editorial_9930677ev.jpg",
         description: "Alternative R&B",
-        date: Temporal.PlainDate.from({year: 2026, month: 7, day: 31}),
+        date: Temporal.PlainDate.from({year: 2026, month: 7, day: 31}).toString(),
         location: "London, UK",
         id: 1
     },
@@ -14,7 +14,7 @@ const gigs = [
         name: "Discovery Zone",
         image: "https://nbhap.com/wp-content/uploads//2020/08/DZ-x-Andie-Riekstina-square-600x600.jpg",
         description: "Experimental pop",
-        date: Temporal.PlainDate.from({year: 2026, month: 10, day: 15}),
+        date: Temporal.PlainDate.from({year: 2026, month: 10, day: 15}).toString(),
         location: "New York, USA",
         id: 2
     },
@@ -22,7 +22,7 @@ const gigs = [
         name: "Ryan Paris",
         image: "https://cdn-images.dzcdn.net/images/artist/d80185b76347931bcd9a851386e54549/1900x1900-000000-80-0-0.jpg",
         description: "Italo Disco",
-        date: Temporal.PlainDate.from({year: 2027, month: 2, day: 3}),
+        date: Temporal.PlainDate.from({year: 2027, month: 2, day: 3}).toString(),
         location: "Rome, IT",
         id: 3
     },
@@ -30,7 +30,7 @@ const gigs = [
         name: "Niagara",
         image: "https://fastly-s3.allmusic.com/artist/mn0000330945/400/K-oL6-OtSnrgT2GAYq3wWz6KsMttLlyBmmVTZ6_CLs0=.jpg",
         description: "Synth-pop",
-        date: Temporal.PlainDate.from({year: 2026, month: 6, day: 10}),
+        date: Temporal.PlainDate.from({year: 2026, month: 6, day: 10}).toString(),
         location: "Paris, FR",
         id: 4
     }
