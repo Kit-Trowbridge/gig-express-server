5de0296a3b0a08c74f7b3e0e9a5e32fcfadd62f2
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-29 10:35:52 +0100

    making gig object

--- app.js
+++ app.js
@@ -6,4 +6,39 @@ app.use(express.json())
 
 app.use(cors());
 
+const gigs = [
+  { 
+    name: "Blood Orange",
+    image: "https://media.vogue.co.uk/photos/5f7af392a67cbe91f90e282b/2:3/w_2560%2Cc_limit/shutterstock_editorial_9930677ev.jpg",
+    description: "Alternative R&B",
+    date: Temporal.PlainDate.from({year: 2026, month: 7, day: 31}),
+    location: "London, UK",
+    id: 1
+  },
+  { 
+    name: "Discovery Zone",
+    image: "https://nbhap.com/wp-content/uploads//2020/08/DZ-x-Andie-Riekstina-square-600x600.jpg",
+    description: "Experimental pop",
+    date: Temporal.PlainDate.from({year: 2026, month: 10, day: 15}),
+    location: "New York, USA",
+    id: 2
+  },
+  { 
+    name: "Ryan Paris",
+    image: "https://cdn-images.dzcdn.net/images/artist/d80185b76347931bcd9a851386e54549/1900x1900-000000-80-0-0.jpg",
+    description: "Italo Disco",
+    date: Temporal.PlainDate.from({year: 2027, month: 2, day: 3}),
+    location: "Rome, IT",
+    id: 3
+  },
+  { 
+    name: "Niagara",
+    image: "https://cdn-images.dzcdn.net/images/artist/d80185b76347931bcd9a851386e54549/1900x1900-000000-80-0-0.jpg",
+    description: "Synth-pop",
+    date: Temporal.PlainDate.from({year: 2026, month: 6, day: 10}),
+    location: "Paris, FR",
+    id: 4
+  }
+] 
+
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const request = require("supertest");
 
-// describe("/gigs", () => {
-//     test("/GET, it responds with an object with gigs as the key and an array of gigs as the value" , async () => {
-
-//     })
-// })
+describe("/gigs", () => {
+    test("/GET, it responds with an object with gigs as the key and an array of gigs as the value" , async () => {
+        const response = await request(app).get("http://localhost:3000/gigs");
+        expect(response.body).toEqual();
+        expect(response.status).toBe(200);
+    })
+})
