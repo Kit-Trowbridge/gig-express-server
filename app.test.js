e7299f5b837290c0da87f33dc44c32ef1c52d7bb
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-29 10:09:39 +0100

    install jest and supertest, export and require app in test file

---
+++ app.test.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const request = require("supertest");
+
+// describe("/gigs", () => {
+//     test("/GET, it responds with an object with gigs as the key and an array of gigs as the value" , async () => {
+
+//     })
+// })
